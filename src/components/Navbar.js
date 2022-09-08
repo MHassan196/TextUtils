23c409f15012bb7f2e745e8d3d1841679253c6cf
MHassan196 <hssnkhatri2@gmail.com>
2022-09-08 11:04:00 +0500

    Chaning the title dynamically

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,42 +2,60 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 export default function Navbar(props) {
-    return (
-        <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
-            <div className="container-fluid">
-                <a className="navbar-brand" href="/">{props.title}</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="/">Home</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="/">{props.aboutText}</a>
-                        </li>
-                    </ul>
-                    {/* <form className="d-flex">
+  return (
+    <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/">{props.title}</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="/">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/">{props.aboutText}</a>
+            </li>
+          </ul>
+          {/* <form className="d-flex">
                         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
                         <button cla
                         ssName="btn btn-primary" type="submit">Search</button>
                     </form> */}
-                    <div className={`form-check form-switch text-${props.mode==='light'?'dark':'light'}`}>
-                        <input className="form-check-input" onClick={props.toggleMode} type="checkbox" id="flexSwitchCheckDefault"/>
-                        <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Enable {props.mode==='light'?'Dark':'Light'} Mode</label>
-                    </div>
-                </div>
-            </div>
-        </nav>
-    )
+          <div className={`form-check mx-3 text-${props.mode === 'light' ? 'dark' : 'light'}`}>
+            <input className="form-check-input" onClick={props.toggleGreen} type="radio" name="flexRadioDefault" id="flexRadioDefault1"/>
+              <label className="form-check-label" htmlFor="flexRadioDefault1">
+                Dark Green
+              </label>
+          </div>
+          <div className={`form-check mx-3 text-${props.mode === 'light' ? 'dark' : 'light'}`}>
+            <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1"/>
+              <label className="form-check-label" htmlFor="flexRadioDefault1">
+                Dark Yellow
+              </label>
+          </div>
+          <div className={`form-check mx-3 text-${props.mode === 'light' ? 'dark' : 'light'}`}>
+            <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1"/>
+              <label className="form-check-label" htmlFor="flexRadioDefault1">
+                Dark red
+              </label>
+          </div>
+          <div className={`form-check form-switch text-${props.mode === 'light' ? 'dark' : 'light'}`}>
+            <input className="form-check-input" onClick={props.toggleMode} type="checkbox" id="flexSwitchCheckDefault" />
+            <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Enable {props.mode === 'light' ? 'Dark' : 'Light'} Mode</label>
+          </div>
+        </div>
+      </div>
+    </nav>
+  )
 }
 
 Navbar.propTypes = {
-    title: PropTypes.string.isRequired,
-    aboutText: PropTypes.string.isRequired
+  title: PropTypes.string.isRequired,
+  aboutText: PropTypes.string.isRequired
 }
 Navbar.defaultProps = {
-    title: 'Set Title Here',
-    aboutText: 'About text here'
+  title: 'Set Title Here',
+  aboutText: 'About text here'
 };
